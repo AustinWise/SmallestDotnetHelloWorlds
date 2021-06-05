1487ca7d1f92db4c10d24ab6aa8dc6ea1ea20b1d
Austin Wise <AustinWise@gmail.com>
2021-06-05 13:29:07 -0700

    add no app host version

---
+++ 4.1.NoAppHost/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _41.NoAppHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
